54f9e2e0f68c37c5fe1ad3227976398098d6a596
Prudhvi Boyapalli <prrb02@gmail.com>
2024-12-29 15:24:38 -0800

    various fixes

--- generate.sh
+++ generate.sh
@@ -4,23 +4,26 @@ echo "Building blog index..."
 
 # echo "<link rel=\"stylesheet\" href=\"./style.css\">\n\n" > ./src/blog.md
 echo "---\ntitle: Blog\n---" > ./src/blog.md
-echo "## Blog\n" >> ./src/blog.md
 for f in $(ls -r src/posts/*.md)
 do
     echo "\t$f";
-    fname=$(echo "$f" | sed 's|^src|build|; s|\.md$|.html|')
-    echo "[$(head -n 1 $f | sed 's/# //')]($fname)\n" >> ./src/blog.md
+    fname=$(echo "$f" | sed 's|^src||; s|\.md$|.html|')
+    title=$(grep '^title:' $f | cut -d ' ' -f 2-)
+    date=$(grep '^date:' $f | cut -d ' ' -f 2-)
+    echo "$date | [$title]($fname)\n" >> ./src/blog.md
 done
+
+echo "Removing old html files..."
+find ./build -name "*.html" -type f -delete
 
 echo "Generating html..."
 for f in $(ls -r src/*.md src/posts/*.md)
 do
     fname=$(echo "$f" | sed 's|^src|build|; s|\.md$|.html|')
     echo "\tgenerating $fname"
-    echo "<link rel=\"stylesheet\" href=\"../style.css\">" > $fname
-    cat src/sidebar.template >> $fname
+    cat src/sidebar.template > $fname
     # need to use "-f markdown-smart" so that quote marks and other ligatures look correct
-    pandoc -f markdown-smart -t html $f | sed 's/↩/\&\#x21A9;/g' >> $fname
+    pandoc -s -c /style.css -f markdown-smart -t html $f | sed 's/↩/\&\#x21A9;/g' >> $fname
 done
 
 # TODO: use a custom html template
--- src/blog.md
+++ src/blog.md
@@ -1,15 +1,13 @@
 ---
 title: Blog
 ---
-## Blog
+2015-07-19 | [OMPL Web Development - Part 5](/posts/2015-07-19-OMPLDev5.html)
 
-[OMPL Web Development - Part 5](build/posts/2015-07-19-OMPLDev5.html)
+2015-07-07 | [OMPL Web Development - Part 4](/posts/2015-07-07-OMPLDev4.html)
 
-[OMPL Web Development - Part 4](build/posts/2015-07-07-OMPLDev4.html)
+2015-07-07 | [OMPL Web Development - Part 3](/posts/2015-07-07-OMPLDev3.html)
 
-[OMPL Web Development - Part 3](build/posts/2015-07-07-OMPLDev3.html)
+2015-06-16 | [OMPL Web Development - Part 2](/posts/2015-06-16-OMPLDev2.html)
 
-[OMPL Web Development - Part 2](build/posts/2015-06-16-OMPLDev2.html)
-
-[OMPL Web Development - Part 1](build/posts/2015-06-08-OMPLDev1.html)
+2015-06-08 | [OMPL Web Development - Part 1](/posts/2015-06-08-OMPLDev1.html)
 
